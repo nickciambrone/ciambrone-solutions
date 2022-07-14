94d8facaf5a3336d5a797f7486773969831e87b1
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2022-07-14 17:03:13 -0400

    conver to router

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './about.styles.scss'
+import { useNavigate } from 'react-router-dom';
 
 const About = () =>{
+    let navigate = useNavigate();
 
     return(
         <div className = 'about'  style={{ color:'white', textAlign:'left'}}>
@@ -11,7 +13,7 @@ const About = () =>{
             <p className = 'bio'>I am a developer specializing in building clean user interfaces for businesses. Whatever your idea is, I will turn it into a reality. 
             My background is in financial technology so you can depend on me to write the code necessary to charge your customers and get paid.  </p>
             <div className="fadeup-enter-done" style={{transitionDelay: "500ms"}}>
-                <a className="email-link"  href="/contact">Contact me</a>
+                <div className="email-link"  onClick={() => navigate("/contact")}>Contact me</div>
             </div>
         </div>
     )
